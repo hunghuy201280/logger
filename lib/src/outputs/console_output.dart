72ec926ed10f51371b8a85c65583c0917761d0b0
Truong Hung Huy <70798373+hunghuy201280@users.noreply.github.com>
2022-10-28 11:41:54 +0700

    Update console_output.dart

--- lib/src/outputs/console_output.dart
+++ lib/src/outputs/console_output.dart
@@ -1,12 +1,25 @@
-import 'package:logger/src/logger.dart';
-import 'package:logger/src/log_output.dart';
 
+import 'dart:io';
+import 'dart:developer' as developer;
+import 'package:flutter/foundation.dart';
+import 'package:idealab/plugins/logger/log_output.dart';
+import 'package:idealab/plugins/logger/logger.dart';
+
+const bool _kReleaseMode = kReleaseMode;
 /// Default implementation of [LogOutput].
 ///
 /// It sends everything to the system console.
 class ConsoleOutput extends LogOutput {
-  @override
+  @OverRide
   void output(OutputEvent event) {
-    event.lines.forEach(print);
+    if (_kReleaseMode || !Platform.isIOS )
+    {
+      event.lines.forEach(debugPrint);
+    }
+    else
+    {
+      event.lines.forEach(developer.log);
+    }
+
   }
 }
