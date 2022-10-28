380c28c0cc3931159c8b44dd55f8a3ee03da3394
huyth12 <huyth12@viettel.com.vn>
2022-10-28 11:58:44 +0700

    update

--- lib/src/outputs/console_output.dart
+++ lib/src/outputs/console_output.dart
@@ -1,25 +1,22 @@
-
-import 'dart:io';
 import 'dart:developer' as developer;
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
-import 'package:logger/src/logger.dart';
 import 'package:logger/src/log_output.dart';
+import 'package:logger/src/logger.dart';
 
 const bool _kReleaseMode = kReleaseMode;
+
 /// Default implementation of [LogOutput].
 ///
 /// It sends everything to the system console.
 class ConsoleOutput extends LogOutput {
-  @OverRide
+  @override
   void output(OutputEvent event) {
-    if (_kReleaseMode || !Platform.isIOS )
-    {
+    if (_kReleaseMode || !Platform.isIOS) {
       event.lines.forEach(debugPrint);
-    }
-    else
-    {
+    } else {
       event.lines.forEach(developer.log);
     }
-
   }
 }
